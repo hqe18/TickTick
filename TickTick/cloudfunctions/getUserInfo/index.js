117a189ffc18ed50143bc607e7aa4baed5272c2d
hqe18 <48673318+hqe18@users.noreply.github.com>
2021-11-21 13:14:49 +0800

    Add files via upload

---
+++ TickTick/cloudfunctions/getUserInfo/index.js
@@ -0,0 +1,14 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+
+cloud.init()
+const db = cloud.database()
+
+// 云函数入口函数
+exports.main = async (event, context) => {
+  //从前端页面传递userInfo，看看在不在数据库中
+  var userInfo = event.userInfo;
+  return await db.collection("user").where({
+    _openid:userInfo.openid
+  }).get()
+}
